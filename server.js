98377b4098dd1acb8f500ca2db12c72ad9fff36e
Ievgenii Spitsyn <ievgeny@zoomdata.com>
2020-09-01 00:34:15 +0300

    Server improvement

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!\n');
 });
